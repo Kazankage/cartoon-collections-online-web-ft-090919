32c87cea0dc4317ea27d21afb884ee1338ac7245
Randel Jiao <randeljiao@gmail.com>
2019-10-03 04:38:41 +0000

    Automatically backed up by Learn

--- ger.rb
+++ ger.rb
@@ -15,7 +15,7 @@ end
 #summon_captain_planet(planeteer_calls)
 
 def long_planeteer_calls(array)
-  puts array.find {|word| if word.length > 4  true}
+  puts array.find {|word| if word.length > 4 }
 end
 
 long_planeteer_calls(names)
