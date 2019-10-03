ffe5b78e006bc13f91314fc3b6fc8dbd2f2dbbab
Randel Jiao <randeljiao@gmail.com>
2019-10-03 04:15:04 +0000

    Automatically backed up by Learn

--- ger.rb
+++ ger.rb
@@ -4,8 +4,12 @@ def roll_call_dwarves(array)
   array.each_with_index do |name, index| puts "#{index + 1} #{name}" end
 end
 
-roll_call_dwarves(names)
+#roll_call_dwarves(names)
 
+
+planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
 def summon_captain_planet(array)
-  
+  array.collect.capitalize
 end
+
+summon_captain_planet(planeteer_calls)
