3cf9914a52a7657ef956bd0eee3b7e4dd113225f
Randel Jiao <randeljiao@gmail.com>
2019-10-03 04:31:47 +0000

    Automatically backed up by Learn

--- ger.rb
+++ ger.rb
@@ -12,4 +12,9 @@ def summon_captain_planet(array)
  print array.collect {|word| word.capitalize + "!"}
 end
 
-summon_captain_planet(planeteer_calls)
+#summon_captain_planet(planeteer_calls)
+
+def long_planeteer_calls(array)
+  array.find
+  
+end
