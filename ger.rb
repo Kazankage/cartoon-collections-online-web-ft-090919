ce41c521fc82016a4c7272ddb8f927f5d4954764
Randel Jiao <randeljiao@gmail.com>
2019-10-03 04:29:36 +0000

    Automatically backed up by Learn

--- ger.rb
+++ ger.rb
@@ -9,7 +9,7 @@ end
 
 planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
 def summon_captain_planet(array)
- print array.collect {|word| (word.capitalize + "!").split}
+ print array.collect {|word| (word.capitalize + "!")}
 end
 
 summon_captain_planet(planeteer_calls)
