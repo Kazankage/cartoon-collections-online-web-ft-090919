be9dfd6d80395f9c43ae7d970f7bd42571a46541
Randel Jiao <randeljiao@gmail.com>
2019-10-03 04:12:08 +0000

    Automatically backed up by Learn

--- ger.rb
+++ ger.rb
@@ -3,3 +3,5 @@ names = ["Doc", "Dopey", "Bashful", "Grumpy"]
 def roll_call_dwarves(array)
   array.each_with_index do |name, index| puts "#{array.index + 1} #{name}"
 end
+
+roll_call_dwarves(names)
