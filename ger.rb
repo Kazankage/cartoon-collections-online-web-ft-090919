11f199993da4bb5f11faae35805b9c6fda37cb24
Randel Jiao <randeljiao@gmail.com>
2019-10-03 04:21:20 +0000

    Automatically backed up by Learn

--- ger.rb
+++ ger.rb
@@ -9,7 +9,7 @@ end
 
 planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
 def summon_captain_planet(array)
-  puts array.join
+  puts array.collect {
 end
 
 summon_captain_planet(planeteer_calls)
